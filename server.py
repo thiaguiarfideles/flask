5b31cb63778ad55502c7a457c3ecb82212d8e958
thiaguiarfideles <30662500+thiaguiarfideles@users.noreply.github.com>
2020-12-07 20:50:43 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
-import pyodbc 
+# from requests import Response
+import pyodbc
 import json
 app = Flask(__name__)
 
@@ -10,21 +11,29 @@ def table():
     username = 'sa' 
     password = 'masterkey' 
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    resultado =[]
     cursor = cnxn.cursor()
 
-    for row in cursor.execute("select PRODUTOS, NM_PRODUTO, CD_ESTOQUE, DS_ESTOQUE, DS_UNIDADE, QT_ESTOQUE, CD_MULTI_EMPRESA, DS_MULTI_EMPRESA from dbo.Estoque_prontonil"):PRODUTOS, NM_PRODUTO, CD_ESTOQUE, DS_ESTOQUE, DS_UNIDADE, QT_ESTOQUE, CD_MULTI_EMPRESA, DS_MULTI_EMPRESA = row
+    ## você sabe o que isso aqui faz? isso ai roda a query do sql server 
+    ## não, faz mais que isso rodar a query é só cursor.execute, então pode tirar 
+    #pois é né
+
+
+    # for row in cursor.execute("select PRODUTOS, NM_PRODUTO, CD_ESTOQUE, DS_ESTOQUE, DS_UNIDADE, QT_ESTOQUE, CD_MULTI_EMPRESA, DS_MULTI_EMPRESA from dbo.Estoque_prontonil"):PRODUTOS, NM_PRODUTO, CD_ESTOQUE, DS_ESTOQUE, DS_UNIDADE, QT_ESTOQUE, CD_MULTI_EMPRESA, DS_MULTI_EMPRESA = row
+        # resultado.append(row)
+    cursor.execute("select PRODUTOS, NM_PRODUTO, CD_ESTOQUE, DS_ESTOQUE, DS_UNIDADE, QT_ESTOQUE, CD_MULTI_EMPRESA, DS_MULTI_EMPRESA from dbo.Estoque_prontonil WITH(NOLOCK)")
+    
     rv = cursor.fetchall() 
-    Estoque_prontonil =[PRODUTOS, NM_PRODUTO, CD_ESTOQUE, DS_ESTOQUE, DS_UNIDADE, QT_ESTOQUE, CD_MULTI_EMPRESA, DS_MULTI_EMPRESA]
-    content = {}
+
+    Estoque_prontonil =[]
+
     for result in rv:
-          content = {'produtos': result['str(produtos)'], 'nm_produto': result['nm_produto'], 'cd_estoque': result['cd_estoque'], 'ds_estoque': result['ds_estoque'],  'ds_unidade': result['ds_unidade'],'ds_unidade': result['ds_unidade'],'qt_estoque': result['qt_estoque'], 'cd_multi_empresa': result['cd_multi_empresa'], 'ds_multi_empresa': result['ds_multi_empresa']}
+          # content = {'produtos': result[0], 'nm_produto': result['nm_produto'], 'cd_estoque': result['cd_estoque'], 'ds_estoque': result['ds_estoque'],  'ds_unidade': result['ds_unidade'],'ds_unidade': result['ds_unidade'],'qt_estoque': result['qt_estoque'], 'cd_multi_empresa': result['cd_multi_empresa'], 'ds_multi_empresa': result['ds_multi_empresa']}}
+          content = {'produtos': result[0], 'nm_produto': result[1], 'cd_estoque': result[2], 'ds_estoque': result[3],  'ds_unidade': result[4],'qt_estoque': result[5], 'cd_multi_empresa': result[6], 'ds_multi_empresa': result[7]}
+          # 
           Estoque_prontonil.append(content)
           content = {}
-    return jsonify (Estoque_prontonil)
-         
-
-     
-
+    return jsonify(prontonil=Estoque_prontonil)
 
 if __name__ == '__main__':
     app.run(debug=True)
